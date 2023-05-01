dce227346397ae8a5e4ffcc7d2d035f063892fdd
Dennis Kotsyuba <Deniskotsyuba17@gmail.com>
2023-05-01 22:18:45 +0300

    merge-3

--- app.js
+++ app.js
@@ -167,6 +167,7 @@ app.use((err, req, res, next) => {
 });
 
 
-app.listen(3000, () => {
-    console.log('Serving on port 3000')
-})
+// app.listen(3000, () => {
+//     console.log('Serving on port 3000')
+// })
+module.exports = app;
