b5135ee0d590e829cafdf5c2c99935e6793aa7ab
Dennis Kotsyuba <Deniskotsyuba17@gmail.com>
2023-05-01 22:39:00 +0300

    Commit-4

--- app.js
+++ app.js
@@ -167,7 +167,7 @@ app.use((err, req, res, next) => {
 });
 
 
-// app.listen(3000, () => {
-//     console.log('Serving on port 3000')
-// })
-module.exports = app;
+app.listen(3000, () => {
+    console.log('Serving on port 3000')
+})
+// module.exports = app;
